9eb5941f0407d6b9df76a4227ac17fea8e2c052a
NguyenMinhTan <101338037+TayHaiSaoTroi@users.noreply.github.com>
2023-12-06 14:37:01 +0700

    Update math_utils.h

--- math_utils.h
+++ math_utils.h
@@ -1,5 +1,5 @@
-#ifndef MATH_UTILS_H
-#define MATH_UTILS_H
+#ifndef MATH_UTILS_H              ifndef dùng để kiểm tra 1 macro đã dược định nghĩa hay chưa, nếu chưa định nghĩa thì nó sẽ chạy tiếp tục
+#define MATH_UTILS_H                   các tác vụ cho đến endif    
 int add(int a, int b);
 
 #endif // Kết thúc của #ifndef
